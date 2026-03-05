d0922533d3e2866a938303c295072865c580e2d4
Jazzysince1992 <47208293+Jazzysince1992@users.noreply.github.com>
2026-03-05 02:42:00 +0000

    Just checking

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.markdown("# Hello")
 st.markdown("## Hello")
-st.markdown("### Hello")
-st.markdown("#### Hello")
-st.markdown("##### Hello")
-st.markdown("##### Hello")
-st.markdown("###### Hello")
+st.header("It is a header")
+st.subheader("It is a subheader")
+st.text("It is a text")
+st.caption("It is a Caption")
+st.latex("x^3+x^2+2x+1")
