fb9d79c25511f1a9e5473af68589a9c678dcf306
Jazzysince1992 <47208293+Jazzysince1992@users.noreply.github.com>
2026-03-05 03:28:24 +0000

    With Dropdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,24 @@ st.subheader("It is a subheader")
 st.text("It is a text")
 st.caption("It is a Caption")
 st.latex("x^3+x^2+2x+1")
+list1 = ["Saurabh","Prasad","Jaiswal"]
+dict = {
+    "fName":"Saurabh",
+    "mName":"Prasad",
+    "lName":"Jaiswal"
+}
+st.header(list1)
+st.header(dict)
+pythonCode = """
+def myFunc(){
+    print("streamlit")
+}
+"""
+
+st.code(pythonCode,language="python")
+
+st.selectbox(
+    label = "Name",
+    options = list1,
+    label_visibility= "visible"
+)
